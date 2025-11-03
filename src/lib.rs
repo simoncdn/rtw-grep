944cb1b25a85c4e50954d6073b754175f3b978ce
Simon Cardona <simon.cdna@proton.me>
2025-11-03 17:47:51 +0100

    feat: add basic search

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,41 @@
 pub mod config;
 
-use std::{error::Error, fs};
 use crate::config::Config;
+use std::{error::Error, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let read_file = fs::read_to_string(config.file_path)?;
 
-    println!("file: {:?}", read_file);
+    for line in search(&config.query, &read_file) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line)
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
