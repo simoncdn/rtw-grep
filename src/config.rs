f8c20480cd6f87346ffe098d494ca46c9c61ff95
Simon Cardona <simon.cdna@proton.me>
2025-11-05 13:49:22 +0100

    feat: case insensitive on searching

--- src/config.rs
+++ src/config.rs
@@ -6,11 +6,16 @@ use std::process;
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub case_insensitive: bool,
 }
 
 impl Config {
-    fn new(query: String, file_path: String) -> Config {
-        Config { query, file_path }
+    fn new(query: String, file_path: String, case_insensitive: bool) -> Config {
+        Config {
+            query,
+            file_path,
+            case_insensitive,
+        }
     }
 
     pub fn get_input_config() -> Config {
@@ -29,8 +34,9 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let case_insensitive = env::var("RTW_GREP_CASE_INSENSITIVE").is_err();
 
-        Ok(Config::new(query, file_path))
+        Ok(Config::new(query, file_path, case_insensitive))
     }
 }
 
@@ -40,10 +46,11 @@ mod tests {
 
     #[test]
     fn test_config_new() {
-        let config = Config::new(String::from("Hello"), String::from("file.txt"));
+        let config = Config::new(String::from("Hello"), String::from("file.txt"), false);
 
         assert_eq!(config.query, "Hello");
         assert_eq!(config.file_path, "file.txt");
+        assert_eq!(config.case_insensitive, false);
     }
 
     #[test]
@@ -55,7 +62,7 @@ mod tests {
         ];
 
         let config = Config::build(&args).unwrap();
-        
+
         assert_eq!(config.query, "query");
         assert_eq!(config.file_path, "file.txt");
     }
@@ -73,6 +80,9 @@ mod tests {
         let result = Config::build(&args);
 
         assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), "Usage: rtw-grep <pattern> <path>".to_string());
+        assert_eq!(
+            result.unwrap_err(),
+            "Usage: rtw-grep <pattern> <path>".to_string()
+        );
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,21 @@ use std::{error::Error, fs};
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let read_file = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &read_file) {
-        let highlighted_line = line.replace(&config.query, &config.query.red().bold().to_string());
+    let result = if config.case_insensitive {
+        search_case_insensitive(&config.query, &read_file)
+    } else {
+        search(&config.query, &read_file)
+    };
+
+    for line in result {
+        let highlighted_line = if config.case_insensitive {
+            line.replace(
+                &config.query.to_lowercase(),
+                &config.query.red().bold().to_string(),
+            )
+        } else {
+            line.replace(&config.query, &config.query.red().bold().to_string())
+        };
         println!("{}", highlighted_line);
     }
 
@@ -26,18 +39,47 @@ pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
     result
 }
 
+pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line)
+        }
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
